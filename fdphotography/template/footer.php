4077f4cc26cd7bb2567af9fa8c24e71f52d5170e
Syahrul Nizam <87962931+zcx47@users.noreply.github.com>
2024-01-22 17:06:48 +0700

    Add files via upload

---
+++ fdphotography/template/footer.php
@@ -0,0 +1,71 @@
+        <!-- Footer Start -->
+        <div class="container-fluid footer py-5 wow fadeIn" data-wow-delay="0.1s">
+            <div class="container py-5">
+                <div class="row g-5 justify-content-center">
+                    <div class="col-lg-4 text-start">
+                        <div class="footer-item d-flex flex-column">
+                            <h4 class="mb-4 text-white">Links</h4>
+                            <a href="#" class="btn-link"> Home</a>
+                            <a href="#category" class="btn-link"> Categories</a>
+                            <a href="#price" class="btn-link"> Price</a>
+                            <a href="#portofolio.php" class="btn-link"> Portofolio</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 text-center">
+                        <div class="footer-item">
+                            <h4 class="mb-4 text-white">FD Photography</h4>
+                            <p class="text-white"> Elevating Moments with Artistry and Precision. Your Trusted Partner for Capturing Timeless Memories. </p>
+                            <div class="btn-link d-flex justify-content-center">
+                                <a href="mailto:febradilva@gmail.com" class="btn btn-md-square btn-light btn-light-outline-0 me-2" target="_blank" rel="noopener noreferrer"><i class="fas fa-envelope"></i></a>
+                                <a href="https://wa.me/6282285578685" class="btn btn-md-square btn-light btn-light-outline-0 me-2" target="_blank" rel="noopener noreferrer"><i class="fab fa-whatsapp"></i></a>
+                                <a href="https://www.instagram.com/fdphotography_23/" class="btn btn-md-square btn-light btn-light-outline-0 me-2" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 text-end">
+                    <div class="footer-item d-flex flex-column">
+                            <h4 class="my-4 text-white">Contact Us</h4>
+                            <a href="mailto:febradilva@gmail.com" class="btn-link" target="_blank" rel="noopener noreferrer"><i class="fas fa-envelope text-secondary me-2"></i> febradilva@gmail.com</a>
+                            <a href="https://wa.me/6282285578685" class="btn-link" target="_blank" rel="noopener noreferrer"><i class="fas fa-phone text-secondary me-2" ></i>(+62)82285578685</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End -->
+
+        <!-- Copyright Start -->
+        <div class="container-fluid copyright py-4">
+            <div class="container">
+                <div class="row g-4 align-items-center">
+                    <div class="col-md-6 text-center text-md-start mb-md-0">
+                        <span class="text-light"><a href="#"><i class="fas fa-copyright text-light me-2"></i>FD Photography</a>, All right reserved.</span>
+                    </div>
+                    <!-- <div class="col-md-6 text-center text-md-end text-white">
+                        Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a> Distributed By <a href="https://themewagon.com">ThemeWagon</a>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+        <!-- Copyright End -->
+
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-primary btn-primary-outline-0 btn-md-square back-to-top"><i class="fa fa-arrow-up"></i></a>   
+
+        
+    <!-- JavaScript Libraries -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/landing/lib/wow/wow.min.js"></script>
+    <script src="assets/landing/lib/easing/easing.min.js"></script>
+    <script src="assets/landing/lib/waypoints/waypoints.min.js"></script>
+    <script src="assets/landing/lib/lightbox/js/lightbox.min.js"></script>
+    
+
+    <!-- Template Javascript -->
+    <script src="assets/landing/js/main.js"></script>
+    </body>
+
+</html>
---
+++ fdphotography/template/header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Fd Photography</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="" name="keywords">
+        <meta content="" name="description">
+        <link rel="shortcut icon" href="assets/landing/img/favicon.ico">
+
+        <!-- Google Web Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+        <!-- Icon Font Stylesheet -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+        <!-- Libraries Stylesheet -->
+        <link href="assets/landing/lib/animate/animate.min.css" rel="stylesheet">
+        <link href="assets/landing/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+
+        <!-- Customized Bootstrap Stylesheet -->
+        <link href="assets/landing/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Template Stylesheet -->
+        <link href="assets/landing/css/style.css" rel="stylesheet">
+    </head>
+
+    <body data-bs-spy="scroll" data-bs-target="#navBar" id="weddingHome">
+
+        <!-- Spinner Start -->
+        <div id="spinner" class="show w-100 vh-100 bg-white position-fixed translate-middle top-50 start-50  d-flex align-items-center justify-content-center">
+            <div class="spinner-grow text-primary" role="status"></div>
+        </div>
+        <!-- Spinner End -->
+
+
+        <!-- Navbar start -->
+        <div class="container-fluid sticky-top px-0">
+            <div class="container-fluid">
+                <div class="container px-0">
+                    <nav class="navbar navbar-dark navbar-expand-xl" id="navBar">
+                        <a href="index.php" class="navbar-brand">
+                            <img src="assets/landing/img/logo.jpg" alt="">
+                            <span class="text-white display-6 fw-bold mb-0">FD Photography</span>
+                        </a>
+                        <button class="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                            <span class="fa fa-bars text-primary"></span>
+                        </button>
+                        <div class="collapse navbar-collapse py-3" id="navbarCollapse">
+                            <div class="navbar-nav mx-auto border-top">
+                                <a href="index.php" class="nav-item nav-link active">Home</a>
+                                <a href="#category" class="nav-item nav-link">Categories</a>
+                                <a href="#price" class="nav-item nav-link">Price</a>
+                                <a href="portofolio.php" class="nav-item nav-link">Portofolio</a>
+                            </div>
+                            <div class="d-flex align-items-center flex-nowrap pt-xl-0">
+                                <a class="btn btn-light btn-light-outline-0 mb-2" href="mailto:febradilva@gmail.com" target="_blank" rel="noopener noreferrer"><i class="fas fa-envelope"></i></a>
+                                <a class="btn btn-light btn-light-outline-0 mb-2" href="https://wa.me/6282285578685" target="_blank" rel="noopener noreferrer"><i class="fab fa-whatsapp"></i></a>
+                                <a class="btn btn-light btn-light-outline-0 mb-2" href="https://www.instagram.com/fdphotography_23/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+                            </div>
+                            <div class="d-flex align-items-center flex-nowrap pt-xl-0">
+                                <a class="btn btn-light btn-light-outline-0 mb-2 bg-secondary" href="login.php">Login</a>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <!-- Navbar End -->
